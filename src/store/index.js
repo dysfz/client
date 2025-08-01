55457f31c0606d0c9494ee2bffb1b24d52629c17
dysfz <vietnam3793@gmail.com>
2025-08-02 01:53:47 +0700

    upd

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,14 @@
 //* import axios (to call API)
 const axios = require('axios')
 
-//* set backend (server) url (endpoint)
-const backendUrl = "https://localhost:8080"
+//* set base (server) url (endpoint)
+const baseUrl = "https://expense-tracker-nong.onrender.com"
+const expenseUrl = '${baseUrl}/expenses/'
 
-//* call API from backend
+//* call API from base
 export const getAllExpenses = async () => {
    try {
-      const response = await axios.get(backendUrl);
+      const response = await axios.get(baseUrl);
       return response.data;
    } catch (err) {
       console.error(err);
@@ -17,7 +18,7 @@ export const getAllExpenses = async () => {
 
 export const deleteExpense = async (id) => {
    try {
-      const response = await axios.delete(backendUrl + id);
+      const response = await axios.delete(baseUrl + id);
       return response.data;
    } catch (err) {
       console.error(err);
@@ -27,7 +28,7 @@ export const deleteExpense = async (id) => {
 
 export const deleteAllExpenses = async () => {
    try {
-      const response = await axios.delete(backendUrl);
+      const response = await axios.delete(baseUrl);
       return response.data;
    } catch (err) {
       console.error(err);
@@ -37,7 +38,7 @@ export const deleteAllExpenses = async () => {
 
 export const addNewExpense = async (expense) => {
    try {
-      const response = await axios.post(backendUrl, expense);
+      const response = await axios.post(baseUrl, expense);
       return response.data;
    } catch (err) {
       console.error(err);
@@ -47,7 +48,7 @@ export const addNewExpense = async (expense) => {
 
 export const getExpenseById = async (id) => {
    try {
-      const response = await axios.get(backendUrl + id);
+      const response = await axios.get(baseUrl + id);
       return response.data;
    } catch (err) {
       console.error(err);
@@ -57,7 +58,7 @@ export const getExpenseById = async (id) => {
 
 export const editExpense = async (id, expense) => {
    try {
-      const response = await axios.put(backendUrl + id, expense);
+      const response = await axios.put(baseUrl + id, expense);
       return response.data;
    } catch (err) {
       console.error(err);
